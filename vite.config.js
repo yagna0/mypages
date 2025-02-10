ca6ddb7ed5740a213a47b9bac882416a5de28e29
yagna <yjt25@email.vccs.edu>
2025-02-10 12:50:47 -0500

    adding react

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     base: "/mypages/",
     plugins: [react()],
      build: {
-        outDir: "../docs",
+        outDir: "docs",
         emptyOutDir: true,
         rollupOptions: {
             input: {
