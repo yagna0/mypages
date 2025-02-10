f362e628cbac69aa6dc580e2d4e0be503eb35b34
yagna <yjt25@email.vccs.edu>
2025-02-10 12:46:06 -0500

    adding react

--- about2/reactRoot.jsx
+++ about2/reactRoot.jsx
@@ -1,20 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-
-
-function App() {
-  return <h1>Hello all from React!</h1>;
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const rootElement = document.getElementById('root'); 
-
-  if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(<App/>);
-  } else {
-    console.error("Element with ID 'root' not found!");
-  }
-});
+const root = createRoot(document.querySelector("root"));
+root.render(<h1>"Hello from react"</h1>)
