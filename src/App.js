8f5d510e73b2f63bcebe903f8da2316ef119e4fd
yagna <yjt25@email.vccs.edu>
2025-02-16 22:07:20 -0500

    trying to update npm start

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App!</h1>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+
+        {/* Home Page Content */}
+        <div>
+          
+          <p>This is a static page. You can go to the React-based page by clicking the link below:</p>
+          <a href="/react-page.html">Go to React-based page</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
